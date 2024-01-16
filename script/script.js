f6e96bf5ac13b0ceb5348d2a0037a8619eafb862
davidemarchini <marchini73@gmail.com>
2024-01-16 23:04:54 +0100

    new repo

---
+++ script/script.js
@@ -0,0 +1,35 @@
+
+// 1- Scambia il contenuto di due variabili a e b, in modo che la prima contenga il valore contenuto nella seconda e viceversa:
+
+// let a = "montagna";
+// let b = "mare";
+
+// console.log("variabile a" + a , "variabile b" + b)
+
+// let aiuto = a;
+// b = a;
+// let aiutone = b;
+// a = b;
+
+// console.log(a + b)
+
+// 2- Stampa questo bellissimo alberello in console:
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// ||
+
+let asterisco = ""
+console.log(asterisco)
+
+
+
+for(let i = 0; i < 6; i ++){
+  // asterisco = asterisco + i;
+  asterisco = asterisco + "*";
+console.log(asterisco)
+}
